d48a29f5ef21bc26b180178518e6e2e9807947e6
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-02-26 14:56:45 +0100

    publishing

--- StoryHubAPI/Program.cs
+++ StoryHubAPI/Program.cs
@@ -93,11 +93,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
